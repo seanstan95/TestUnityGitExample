4a6fc7cbf7543f2df775099483a8f5d2c1b22d1b
Sean Stanley <seanstan95@gmail.com>
2023-03-23 14:14:01 -0400

    Initial commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject canvas;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!canvas.activeSelf)
+            {
+                canvas.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+                Time.timeScale = 0;
+            } else
+            {
+                canvas.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    public void OnClick(string buttonName)
+    {
+        if(buttonName == "Exit")
+            UnityEditor.EditorApplication.isPlaying = false;
+        else if(buttonName == "Unpause")
+        {
+            canvas.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Time.timeScale = 1;
+        }
+    }
+}
